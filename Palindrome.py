1fb6a6507ad378c41dc0884da48e3b46bae4962f
Idris-David <153531303+Idris-David@users.noreply.github.com>
2023-12-11 17:25:08 +0100

    Update Palindrome.py

--- Palindrome.py
+++ Palindrome.py
@@ -3,6 +3,7 @@
 
 def palindrome(word):
     word = word.lower()
+    word = word.strip()
     list_string = []
     forward = ""
     backward = ""
@@ -21,10 +22,3 @@ def palindrome(word):
     
 
 palindrome("Okay")
-    
-    #return rightward, backward
-
-#a = int(input('Enter 1st number: '))
-#b = int(input('Enter 2nd number: '))
-
-#print(f'Sum of {a} and {b} is {sum(a, b)}')
